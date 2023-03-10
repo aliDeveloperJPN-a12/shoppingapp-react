d264a870e65e8908e5853e98de5e43018d2d3dcc
aliDeveloperJPN-a12 <115926291+aliDeveloperJPN-a12@users.noreply.github.com>
2023-03-10 06:14:42 -0800

    Updates

---
+++ precache-manifest.5e7671217789370a47c4d5dab043a501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55dc2c7f99e185585d65",
+    "url": "/shoppingapp-react/static/css/main.72c6c19c.chunk.css"
+  },
+  {
+    "revision": "55dc2c7f99e185585d65",
+    "url": "/shoppingapp-react/static/js/main.55dc2c7f.chunk.js"
+  },
+  {
+    "revision": "6e4c421a53937c5281bd",
+    "url": "/shoppingapp-react/static/js/1.6e4c421a.chunk.js"
+  },
+  {
+    "revision": "863dc9da141ce0b361fa",
+    "url": "/shoppingapp-react/static/js/runtime~main.863dc9da.js"
+  },
+  {
+    "revision": "3f5d8d5bd09f630a9feb728972601838",
+    "url": "/shoppingapp-react/static/media/ali ghane fard.3f5d8d5b.jpg"
+  },
+  {
+    "revision": "dd05527ca18cde225ddb95f042b327d1",
+    "url": "/shoppingapp-react/index.html"
+  }
+];
